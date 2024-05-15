f10b243fdc73ef68009642ce09d96d7218f132a8
PatricioPonce28 <168014711+PatricioPonce28@users.noreply.github.com>
2024-05-15 12:02:50 -0500

    Add files via upload

---
+++ EjercicioInstagram.py
@@ -0,0 +1,22 @@
+print("Instagram")
+
+user=input("Phone number, username or email ")
+password = input("Password ")
+
+if user == "Pato" and password == "123" :
+    print(f"Welcome {user}")
+    print("loading your posts")
+else:
+    print("Sorry your password was incorrect, Please double-check your password")
+
+print ("Sign up to see photos and videos from your friends ")
+input("Log in with Facebook ")
+facebook=input("Yes or No")
+if facebook == "Yes" :
+    print ("Welcome")
+else:
+    mobile=input("Mobile number or email ")
+    name=input("Full name ")
+    username=input("Username ")
+    fpassword=("password ")
+    
